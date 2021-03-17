de2dcf32f0e2a3ec144772d057a14bd9271a3766
Kam Low <auscaster@gmail.com>
2021-03-17 01:46:45 +0100

    Remove padding

--- src/base/components/Spinner.vue
+++ src/base/components/Spinner.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <transition> -->
-    <div v-show="show" :class="{ overlay: overlay }" class="text-center py-5">
+    <div v-show="show" :class="{ overlay: overlay }" class="text-center">
       <svg class="spinner" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" :style='{ width: size + "px" }'>
         <circle class="path" cx="25" cy="25" r="23"/>
       </svg>
